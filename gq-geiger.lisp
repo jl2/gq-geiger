612ef34175df584daf29c470c9479ba58106eded
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2024-03-28 23:08:19 -0600

    Move most stuff into gq-geiger.lisp

--- gq-geiger.lisp
+++ gq-geiger.lisp
@@ -15,4 +15,192 @@
 
 (in-package :gq-geiger)
 
-;; TODO: Put stuff here.
+(defun baud-type (n)
+  (find n '(1200
+            2400
+            4800
+            9600
+            14400
+            19200
+            28800
+            38400
+            57600
+            115200)))
+
+(deftype geiger-baud-type ()
+  '(satisfies baud-type))
+
+(defun find-geiger (&key (baud 115200) (timeout-ms 5000))
+   (make-instance 'geiger-counter
+                  :path (car (uiop:directory-files "/dev/" "ttyUSB*"))
+                  :baud baud
+                  :timeout timeout-ms))
+
+
+(defclass geiger-counter ()
+  ((path
+    :initform (car (uiop:directory-files "/dev/" "ttyUSB*"))
+    :initarg :path
+    :type (or string path)
+    :documentation "The path to the GQ Geiger Counter serial device.")
+   (baud
+    :initform 115200
+    :initarg :baud
+    :type geiger-baud-type
+    :documentation "Baud to use when communicating with the geiger counter on the serial port.")
+   (timeout :initform 500
+            :initarg :timeout
+            :type fixnum
+            :documentation "The timeout used to read and write geiger counter commands.")))
+
+(defun geiger (&key (path (car (uiop:directory-files "/dev/" "ttyUSB*"))) (baud 115200) (timeout-ms 500))
+  (make-instance 'geiger-counter
+                 :path path
+                 :baud baud
+                 :timeout timeout-ms))
+
+(declaim (ftype (function
+                 ((simple-array (unsigned-byte 8)) fixnum)
+                 (unsigned-byte 16))
+                get-u2)
+         (inline get-u2)
+
+         (ftype (function
+                 ((simple-array (unsigned-byte 8)) fixnum)
+                 (unsigned-byte 32))
+                get-u4)
+         (inline get-u4)
+
+         (ftype (function
+                 ((simple-array (unsigned-byte 8)) fixnum)
+                 (signed-byte 32))
+                get-s4)
+         (inline get-s4))
+
+(defun get-u2 (arr idx)
+  "Interpret two bytes in arr as an '(unsigned-byte 32)"
+  (declare
+   (optimize (speed 3) (space 0) (safety 0) (debug 0))
+   (type (simple-array (unsigned-byte 8)) arr))
+  (the (unsigned-byte 16) (+ (* (aref arr (1+ idx)) 256) (aref arr idx))))
+
+
+(defun get-u4 (arr idx)
+  "Interpret the four bytes in arr as an '(unsigned-byte 32)"
+  (declare
+   (optimize (speed 3) (space 0) (safety 0) (debug 0))
+   (type (simple-array (unsigned-byte 8)) arr))
+  (the (unsigned-byte 32) (+ (* (+ (* (+ (* (aref arr (+ 0 idx)) 256)
+                                         (aref arr (+ 1 idx))) 256)
+                                   (aref arr (+ 2 idx))) 256)
+                             (aref arr (+ 3 idx)))))
+
+(defun get-s4 (arr idx)
+  "Interpret four bytes in arr as an '(signed-byte 32)"
+  (declare
+   (optimize (speed 3) (space 0) (safety 0) (debug 0))
+   (type (simple-array (unsigned-byte 8)) arr))
+  (the (signed-byte 32) (+ (* (+ (* (+ (* (aref arr (+ 0 idx)) 256)
+                                       (aref arr (+ 1 idx))) 256)
+                                 (aref arr (+ 2 idx))) 256)
+                           (aref arr (+ 3 idx)))))
+
+
+(defun build-command-string (command args)
+  "Create a command string to send to the GQ geiger counter serial device.
+See gq-documentation/GQ-RFC1801.txt for details.  In short, this converts
+ command args -> <command arg1 arg2 arg3 ...>>"
+  (format nil "<~a~{~^ ~x~}>>" (string-upcase command) args))
+
+(defun send-command (geiger command &optional return-type &rest args)
+  "Send a command with arguments to the geiger counter and read a value of the given type in return.
+Type can be:
+  '(:string <n>)    - an ascii string of size <n>
+  '(:array <n>)     - a byte array of size <n>
+  :unsigned-int32   - an unsigned 32-bit integer as a lisp integer type
+  :signed-int32     - a signed 32-bit integer as a lisp integer type
+  :byte             - an 8-bit integer as a lisp integer type
+  nil               - Indicates no return value
+
+Where nil means 'no value'.  '(:string n) an '(:array n) can have any size up to 4096."
+  (with-slots (timeout path baud) geiger
+    (cserial-port:with-serial (sp
+                               path
+                               :baud-rate baud
+                               :data-bits 8
+                               :parity :none
+                               :stop-bits 1
+                               :encoding :ascii)
+
+      (cserial-port:with-timeout (timeout)
+        (let ((cmd (build-command-string command args)))
+          (cserial-port:write-serial-string cmd sp)
+
+          (cond
+            ;; '(:string <size>)
+            ((and (consp return-type)
+                  (eq (car return-type) :string))
+             (let ((string (make-string (cadr return-type))))
+               (cserial-port:read-serial-string string sp)
+               string))
+
+            ;; '(:array <size>)
+            ((and (consp return-type)
+                  (eq (car return-type) :array))
+             (let ((buffer (make-array (cadr return-type)
+                                       :element-type '(unsigned-byte 8))))
+               (cserial-port:read-serial-byte-vector buffer sp)
+               buffer))
+
+            ((eq return-type :unsigned-int32)
+             (let ((buffer (make-array 4
+                                       :element-type '(unsigned-byte 8))))
+               (cserial-port:read-serial-byte-vector buffer sp)
+               (get-u4 buffer 0)))
+
+            ((eq return-type :signed-int32)
+             (let ((buffer (make-array 4
+                                       :element-type '(unsigned-byte 8))))
+               (cserial-port:read-serial-byte-vector buffer sp)
+               (get-s4 buffer 0)))
+
+            ((eq return-type :byte)
+             (cserial-port:read-serial-byte sp))
+
+            ((null return-type)
+             nil)
+
+            (t
+             (error "Unknown return type ~a" return-type))))))))
+
+
+(defun get-version (&optional (geiger (find-geiger)))
+  "Return information about the geiger counter model and firmware revision."
+  (send-command geiger
+                "GETVER"
+                '(:string 15)))
+
+(defun reboot (&optional (geiger (find-geiger)))
+  "Reboot the geiger counter."
+  (send-command geiger
+                "REBOOT"
+                nil))
+
+(defun get-cpm (&optional (geiger (find-geiger)))
+  "Return the current counts per minute (CPM) reading."
+  (send-command geiger
+                "GETCPM"
+                :unsigned-int32))
+
+(defun get-volt (&optional (geiger (find-geiger)))
+  "Get the battery voltage status."
+  (send-command geiger
+                "GETVOLT"
+                '(:string 5)))
+
+(defun get-config (&optional (geiger (find-geiger)))
+  "Return a 512 byte configuration data buffer."
+  (send-command geiger
+                "GETCFG"
+                '(:array 512)))
+
